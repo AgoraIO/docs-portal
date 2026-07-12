07aad9503ddb0866a39ecb320411c46f78802004
czhen <56986964+shczhen@users.noreply.github.com>
2026-07-13 01:46:05 +0800

    fix: exclude Doxygen helper pages (#724)

--- scripts/html-migration/lanes/doxygen-javadoc.mjs
+++ scripts/html-migration/lanes/doxygen-javadoc.mjs
@@ -3,8 +3,24 @@ export const doxygenJavadocLane = {
   filterSourceNames(sourceNames) {
     return sourceNames.filter(isMigratableDoxygenOrJavadocSource);
   },
+  isNavigationSource(sourceName) {
+    return !isGeneratedDoxygenIndex(sourceName);
+  },
   usesSemanticRenderer: true,
 };
+
+const GENERATED_INDEX_PAGES = new Set([
+  'annotated.html',
+  'classes.html',
+  'deprecated.html',
+  'dirs.html',
+  'examples.html',
+  'files.html',
+  'hierarchy.html',
+  'modules.html',
+  'namespaces.html',
+  'pages.html',
+]);
 
 function isMigratableDoxygenOrJavadocSource(sourceName) {
   const basename = sourceName.split('/').at(-1) ?? sourceName;
@@ -16,4 +32,12 @@ function isMigratableDoxygenOrJavadocSource(sourceName) {
   if (/^globals(?:[_-].*)?\.html$/i.test(basename)) return false;
 
   return true;
+}
+
+function isGeneratedDoxygenIndex(sourceName) {
+  const basename = sourceName.split('/').at(-1) ?? sourceName;
+  return (
+    GENERATED_INDEX_PAGES.has(basename.toLowerCase()) ||
+    /^dir[_-].*\.html$/i.test(basename)
+  );
 }
