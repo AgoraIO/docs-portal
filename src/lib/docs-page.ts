1a745fb0390351e8917ed6c0e191c607d64894ec
czhen <56986964+shczhen@users.noreply.github.com>
2026-07-27 18:46:52 +0800

    chore: update remaining dependencies

--- src/lib/docs-page.ts
+++ src/lib/docs-page.ts
@@ -1,14 +1,14 @@
 import { createServerFn } from '@tanstack/react-start';
 
 export const getDocsTabIndex = createServerFn({ method: 'GET' })
-  .inputValidator((data: { locale: string; tab: string }) => data)
+  .validator((data: { locale: string; tab: string }) => data)
   .handler(async ({ data }) => {
     const { loadDocsTabIndex } = await import('./docs-page.server');
     return loadDocsTabIndex(data.locale, data.tab);
   });
 
 export const getDocsPagePayload = createServerFn({ method: 'GET' })
-  .inputValidator(
+  .validator(
     (data: {
       locale: string;
       search?: string;
